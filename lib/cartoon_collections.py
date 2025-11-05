b1958cba0dd9e4826242fc1ce52210d3464b9f01
Dean Baberowski <deanbaberows@gmail.com>
2025-11-05 13:56:27 +1100

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, name in enumerate(dwarves):
+        print(f"{i + 1}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    return [call.capitalize() + "!" for call in calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    if any(len(call) > 4 for call in calls):
+        return True
+    else:
+        return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(food_items):
+    cheese_list = ["cheddar", "gouda", "camembert"]
+    for item in food_items:
+            if item in cheese_list:
+                return item
+    return None
